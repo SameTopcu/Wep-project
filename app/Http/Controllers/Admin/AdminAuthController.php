437671018ca22ce0fd4da809eaa7fba054ca3f02
Abdul Samet TOPCU <topcu.abdulsamet@hotmail.com>
2026-01-23 16:21:14 +0300

    Login ve Dashboard ayarları yapıldı

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -4,12 +4,52 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthController extends Controller
 {
     public function login()
     {
         return view('admin.login');
+    }
+
+    public function login_submit(Request $request){
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ], [
+            'email.required' => 'Email adresi gereklidir.',
+            'email.email' => 'Geçerli bir email adresi giriniz.',
+            'password.required' => 'Şifre gereklidir.',
+        ]);
+    
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+    
+        if(Auth::guard('admin')->attempt($data)) {
+            return redirect()->route('admin_dashboard')->with('success', 'Giriş başarılı!');
+        } else {
+            return redirect()->route('admin_login')->with('error','Girdiğiniz bilgiler hatalı! Lütfen tekrar deneyiniz!');
+        } 
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login')->with('success','Logout is successful!');
+    }
+    public function profile(){
+        return view('admin.profile');
+    }
+
+    public function forget_password(){
+        return view('admin.forget-password');
+    }
+
+    public function reset_password(){
+        return view('admin.reset-password');
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,27 @@
 
 use App\Http\Controllers\Admin\AdminAuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-// Admin
-Route::prefix('admin')->group(function () {
 
+
+Route::middleware('admin')->prefix('admin')->group(callback: function () {
+    Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
+    Route::get('/profile',[AdminDashboardController::class,'profile'])->name('admin_profile');
+});// Admin
+Route::prefix('admin')->group(callback: function () {
 
     Route::get('/login',[AdminAuthController::class,'login'])->name('admin_login');
-
-
-
-
+    Route::post('/login',[AdminAuthController::class,'login_submit'])->name('admin_login_submit');
+    Route::get('/logout',[AdminAuthController::class,'logout'])->name('admin_logout');
+    Route::get('/forget-password',[AdminAuthController::class,'forget_password'])->name('admin_forget_password');
+    Route::post('/forget_password_submit',[AdminAuthController::class,'forget_password_submit'])->name('admin_forget_password_submit');
+    Route::get('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password'])->name('admin_reset_password');
+    Route::post('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password_submit'])->name('admin_reset_password_submit');
 
 
 });
+
